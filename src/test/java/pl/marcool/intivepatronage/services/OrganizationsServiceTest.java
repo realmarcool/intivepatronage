3a1bec06c4b12cffbdac4842ca6b844c8c8700b5
real_marcool <marcul@poczta.onet.pl>
2019-01-25 21:53:58 +0100

    nowe testy

--- src/test/java/pl/marcool/intivepatronage/services/OrganizationsServiceTest.java
+++ src/test/java/pl/marcool/intivepatronage/services/OrganizationsServiceTest.java
@@ -1,37 +1,74 @@
 package pl.marcool.intivepatronage.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.marcool.intivepatronage.models.Organization;
+import pl.marcool.intivepatronage.models.dto.OrganizationDTO;
+import pl.marcool.intivepatronage.models.dto.RoomDTO;
+import pl.marcool.intivepatronage.repositores.OrganizationRepository;
+import pl.marcool.intivepatronage.repositores.RoomRepository;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class OrganizationsServiceTest {
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    OrganizationsService organizationsService;
+
+    OrganizationDTO organization1 = new OrganizationDTO();
+    OrganizationDTO organization2 = new OrganizationDTO();
 
     @Before
     public void setUp() throws Exception {
+        organizationsService = new OrganizationsService(organizationRepository, objectMapper);
+        organization1.setName("Organization1");
+        organizationsService.save(organization1);
     }
 
     @Test
     public void save() {
+        organization2.setName("Org2");
+        organizationsService.save(organization2);
+        assertTrue(organizationsService.getAll().size() == 2);
     }
 
     @Test
     public void getAll() {
+        assertTrue(organizationsService.getAll().size() == 1);
     }
 
     @Test
     public void findById() {
+        assertTrue(organizationsService.findById(organization1.getName()).getName().equals(organization1.getName()));
     }
 
     @Test
     public void update() {
+        organization1.setName("Organization2");
+        organizationsService.update("Organization1", organization1);
+        assertTrue(organizationsService.findById("Organization2").getName().equals("Organization2"));
     }
 
     @Test
     public void deleteById() {
+        organizationsService.deleteById(organization1.getName());
+        assertTrue(organizationsService.getAll().size() == 0);
     }
 
     @Test
     public void deleteAll() {
+        organizationsService.deleteAll();
+        assertTrue(organizationsService.getAll().size() == 0);
     }
 }
--- src/test/java/pl/marcool/intivepatronage/services/RoomsServiceTest.java
+++ src/test/java/pl/marcool/intivepatronage/services/RoomsServiceTest.java
@@ -7,9 +7,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import pl.marcool.intivepatronage.models.Room;
 import pl.marcool.intivepatronage.models.dto.RoomDTO;
 import pl.marcool.intivepatronage.repositores.RoomRepository;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -24,8 +25,6 @@ public class RoomsServiceTest {
 
     RoomDTO room1 = new RoomDTO();
     RoomDTO room2 = new RoomDTO();
-    RoomDTO testDTOtoRoom = new RoomDTO();
-    Room testRoomToDTO = new Room();
 
     @Before
     public void setUp() throws Exception {
@@ -42,42 +41,54 @@ public class RoomsServiceTest {
         room1.setPhoneInNumber(91);
         room1.setPhoneOutNumber("+12 123456789");
         room1.setCommunicationInterface("bluetooth");
-        room1.setName("conference room 2");
-        room1.setId("CR2");
-        room1.setFloor(10);
-        room1.setAvailability(true);
-        room1.setSeating(20);
-        room1.setStandingPlace(20);
-        room1.setLyingPlace(20);
-        room1.setHammock(20);
-        room1.setPhone(true);
-        room1.setPhoneInNumber(91);
-        room1.setPhoneOutNumber("+12 123456789");
-        room1.setCommunicationInterface("bluetooth");
+        room2.setName("conference room 2");
+        room2.setId("CR2");
+        room2.setFloor(10);
+        room2.setAvailability(true);
+        room2.setSeating(20);
+        room2.setStandingPlace(20);
+        room2.setLyingPlace(20);
+        room2.setHammock(20);
+        room2.setPhone(true);
+        room2.setPhoneInNumber(91);
+        room2.setPhoneOutNumber("+12 123456789");
+        room2.setCommunicationInterface("bluetooth");
         roomsService.save(room1);
     }
 
     @Test
     public void save() {
+        roomsService.save(room2);
+        assertTrue(roomsService.getAll().size() == 2);
     }
 
     @Test
     public void getAll() {
+        assertTrue(roomsService.getAll().size() == 1);
     }
 
     @Test
     public void findById() {
+        assertTrue(roomsService.findById(room1.getId()).getId().equals(room1.getId()));
     }
 
     @Test
     public void update() {
+        room1.setId("CR2");
+        room1.setName("Room2");
+        roomsService.update("CR1", room1);
+        assertTrue(roomsService.findById("CR2").getName().equals("Room2"));
     }
 
     @Test
     public void deleteById() {
+        roomsService.deleteById(room1.getId());
+        assertTrue(roomsService.getAll().size() == 0);
     }
 
     @Test
     public void deleteAll() {
+        roomsService.deleteAll();
+        assertTrue(roomsService.getAll().size() == 0);
     }
 }
